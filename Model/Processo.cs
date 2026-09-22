3379b698c26702a23dd04c7ca5c260cffe3296f8
Santana <eliezersantanad44335@gmail.com>
2026-09-22 13:31:57 -0400

    adicionei os codigos no Model processo.cs

--- Model/Processo.cs
+++ Model/Processo.cs
@@ -2,5 +2,12 @@
 {
     public class Processo
     {
+        public int id { get; set; }
+        public string Numero { get; set; }
+        public DateTime? Data { get; set; }
+        public string Interresado { get; set; }=string.Empty;
+        public string Assunto { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string Situacao { get; set; } = string.Empty;
     }
 }
