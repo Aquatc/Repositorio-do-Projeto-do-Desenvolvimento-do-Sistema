ab282139bdf0c2b095a811c805f6df67cddb72f7
Santana <eliezersantanad44335@gmail.com>
2026-09-21 21:49:20 -0400

    fiz o processoDAO e o processo.cs

---
+++ DAO/ProcessoDAO.cs
@@ -0,0 +1,45 @@
+﻿using Biblioteca.Configs;
+using Biblioteca.Model;
+using Bibloteca.Configs;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace Biblioteca.DAO
+{
+    public class ProcessoDAO
+    {
+        private readonly Conexao _conexao;
+
+        public ProcessoDAO(Conexao conexao)
+        {
+            _conexao = conexao;
+        }
+
+        public List<Processo> Listar()
+        {
+            var lista = new List<Processo>();
+            var comando = _conexao.CreateCommand("SELECT * FROM processos;");
+            var leitor = (MySqlDataReader)comando.ExecuteReader();
+
+            while (leitor.Read())
+            {
+                lista.Add(MapearProcesso(leitor));
+            }
+            return lista;
+        }
+
+        private static Processo MapearProcesso(MySqlDataReader leitor)
+        {
+            return new Processo
+            {
+                Id = leitor.GetInt32("id_pro"),
+                Numero = DAOHelper.GetString(leitor, "numero_pro"),
+                Data = DAOHelper.GetDateTime(leitor, "data_pro"),
+                Interessado = DAOHelper.GetString(leitor, "interessado_pro"),
+                Assunto = DAOHelper.GetString(leitor, "assunto_pro"),
+                Descricao = DAOHelper.GetString(leitor, "descricao_pro"),
+                Situacao = DAOHelper.GetString(leitor, "situacao_pro")
+            };
+        }
+    }
+}
---
+++ Model/Processo.cs
@@ -0,0 +1,6 @@
+﻿namespace Bibloteca.Model
+{
+    public class Processo
+    {
+    }
+}
